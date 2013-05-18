be517b7ed222630aef5865ed1f0f4e7df207a2cd
robert <robert@42.co.nz>
2013-05-18 16:03:49 +1200

    Clean up build dirs and commit built hex files

--- Release/libraries/FlexiTimer2/FlexiTimer2.d
+++
@@ -1,5 +0,0 @@
-libraries/FlexiTimer2/FlexiTimer2.d: \
- ../libraries/FlexiTimer2/FlexiTimer2.cpp \
- /home/robert/gitrep/freeboardPLC/libraries/FlexiTimer2/FlexiTimer2.h
-
-/home/robert/gitrep/freeboardPLC/libraries/FlexiTimer2/FlexiTimer2.h:
--- Release/libraries/PinCatcher/PinCatcher.d
+++
@@ -1,7 +0,0 @@
-libraries/PinCatcher/PinCatcher.d: ../libraries/PinCatcher/PinCatcher.cpp \
- ../libraries/PinCatcher/PinCatcher_impl.h \
- ../libraries/PinCatcher/PinCatcher.h
-
-../libraries/PinCatcher/PinCatcher_impl.h:
-
-../libraries/PinCatcher/PinCatcher.h:
